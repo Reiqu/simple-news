5c24288c6b2879ca22daa6e8e6e6d927b6123b1f
Adrian <reiqu@outlook.de>
2018-09-05 16:49:51 +0200

    Added filter but no functionality

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,13 +8,24 @@ import { NewsService } from '../news.service';
 })
 export class HomeComponent implements OnInit {
   news: any = [];
+  countries = [{id: 0, code: 'de', name: 'Deutschland'}, {id: 1, code: 'usa', name: 'USA'}, {id: 2, code: 'uk', name: 'Großbritannien'}];
+  categories = [{id: 0, category: 'business', name: 'Wirtschaft'}, {id: 1, category: 'entertainment', name: 'Unterhaltung'},
+    {id: 2, category: 'health', name: 'Gesundheit'}, {id: 3, category: 'science', name: 'Wissenschaft'},
+    {id: 4, category: 'sports', name: 'Gesundheit'}, {id: 5, category: 'technology', name: 'Technologoie'}];
+  selectedCategory: string;
+  selectedCountry: string;
+
   constructor(private newsservice: NewsService) {
+
   }
   showNews(country: string, category: string) {
     this.newsservice.getNews(country, category)
       .subscribe(data => {
         return this.news = Object.values(data)[2];
       });
+  }
+  onChangeNews() {
+    this.showNews(this.selectedCountry, this.selectedCategory);
   }
 
     ngOnInit() {
