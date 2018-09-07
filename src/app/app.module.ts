58812ddf8adb180634444a492dc5497289ba7239
Adrian <cgdoerpen@gmail.com>
2018-09-07 17:45:21 +0200

    Added CookieService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { CookieService } from 'ngx-cookie-service';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AccountLoginComponent } from './account-login/account-login.component';
 import { AccountDetailsComponent } from './account-details/account-details.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,9 +23,10 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
